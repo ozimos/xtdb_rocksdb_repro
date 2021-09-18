186e42b7b3ba9e344d0e62083ad71de95f2a09b4
Tovieye Ozi <tovieye.ozi@gmail.com>
2021-09-18 14:23:00 +0100

    cannot repro

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 Error Repro Repo for SIGSEV on XTDB RocksDB Index
 
+
+## Setup
+
+Create a docker postgres DB for the repo. For more info see this article https://towardsdatascience.com/how-to-run-postgresql-and-pgadmin-using-docker-3a6a8ae918b5
+
+A docker-compose file is provided in the repo
+Run the command below in the root repo directory
+
+    $ docker compose up
+
 ## Usage
 
 The SIGSEV happens during testing
--- src/ozimos/xtdb_rocksdb_repro.clj
+++ src/ozimos/xtdb_rocksdb_repro.clj
@@ -2,23 +2,27 @@
   (:require [ozimos.xtdb :as xtdb]
             [xtdb.api :as xt]
             [hato.client :as hc]
-            [ozimos.client :refer [client]]
-            [mount.core :as mount]))
+            [crypticbutter.snoop :as snoop :refer [>defn =>]]
+            [ozimos.client :refer [client]]))
 
 (set! *warn-on-reflection* true)
 
-(defn repro [eid] 
-  (let [res (:body (hc/get "http://localhost:7655/api" {:client client :as :json}))
-        tx (xt/submit-tx xtdb/xtdb [[::xt/put (assoc res :xt/id eid)]])]
-    (xt/await-tx xtdb/xtdb tx)
-    (xt/entity (xt/db xtdb/xtdb) eid)))
+(def SampleResponse [:map 
+                     [:xt/id keyword?]
+                     [:color string?]
+                     [:name string?]
+                     [:size int?]])
+(>defn repro
+       {::snoop/macro-config {:log-fn-sym 'taoensso.timbre/error}}
+       [eid]
+       [keyword? => SampleResponse]
+       (let [res (:body (hc/get "http://localhost:7655/api" {:client client :as :json}))
+             tx (xt/submit-tx xtdb/xtdb [[::xt/put (assoc res :xt/id eid)]])]
+         (xt/await-tx xtdb/xtdb tx)
+         (xt/entity (xt/db xtdb/xtdb) eid)))
 
 (defn fetch-data []
    (:body (hc/get "http://localhost:7655/api" {:client client :as :json})))
 
 
 
-(comment
-  (mount/start)
-  (mount/stop)
-  )
